2b15cf247e0869b7ce338c49d673a6f34b5b375e
“Chi <bennylin77@gmail.com>
2018-07-07 16:25:49 +0000

    update

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+	def index
+	end
+	def about
+	end
+	def methods
+	end
+	def categories
+		#@category = params[:category]
+	end
+	def maintenance
+	end
+end
